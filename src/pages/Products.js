0cee318ca20fd8df2aa5006e75d409f49adefc71
DinhSonHai <sdinhhai@gmail.com>
2020-08-31 15:46:25 +0700

    Get data from json server

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Container, Row, Col, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class Products extends Component {
     constructor(props) {
         super(props);
 
-        
+        this.state = {
+          products: [],
+        }
+    }
+    componentDidMount() {
+      axios.get('http://localhost:8080/products')
+        .then(res => {
+          this.setState({
+            products: res.data
+          });
+        })
     }
     render() {
         const { products } = this.state;
