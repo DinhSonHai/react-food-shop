95c186723ec0de37967be0245b9c31f8362054d1
DinhSonHai <sdinhhai@gmail.com>
2020-08-31 14:26:38 +0700

    Add route

---
+++ src/components/TopMenu.js
@@ -0,0 +1,76 @@
+import React, { useState, Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+class TopMenu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false,
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        })
+    }
+    render() {
+        return (
+        <div>
+            <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarToggler onClick={ this.toggle } />
+            <Collapse isOpen={ this.state.isOpen } navbar>
+                <Nav className="mr-auto" navbar>
+                <NavItem>
+                    <NavLink>
+                        <Link to="/">Home</Link>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <Link to="/products">Product</Link>
+                    </NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                    Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                    <DropdownItem>
+                        Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                        Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                        Reset
+                    </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+                </Nav>
+                <NavbarText>Simple Text</NavbarText>
+            </Collapse>
+            </Navbar>
+        </div>
+        );
+    }
+}
+
+export default TopMenu;
